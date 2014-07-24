2ba7a4ca5b9bd31c3a94c043a20442f591f550c2
Mike Marcacci <mike.marcacci@thecontrolgroup.com>
2014-07-23 22:38:27 -0700

    Simplify to use the SET key value PX 123 NX syntax

--- index.js
+++ index.js
@@ -8,43 +8,9 @@ function acquireLock(client, lockName, timeout, retryDelay, onLockAquired) {
 	}
 
 	var lockTimeoutValue = (Date.now() + timeout + 1);
-
-	client.setnx(lockName, lockTimeoutValue, function(err, result) {
-		if(err) return retry();
-
-		if(result === 0) {
-			// Lock couldn't be aquired. Check if the existing lock has timed out.
-
-			client.get(lockName, function(err, existingLockTimestamp) {
-				if(err) return retry();
-				if(!existingLockTimestamp) {
-					// Wait, the lock doesn't exist!
-					// Someone must have called .del after we called .setnx but before .get.
-					// https://github.com/errorception/redis-lock/pull/4
-					return retry();
-				}
-
-				existingLockTimestamp = parseFloat(existingLockTimestamp);
-
-				if(existingLockTimestamp > Date.now()) {
-					// Lock looks valid so far. Wait some more time.
-					return retry();
-				}
-
-				lockTimeoutValue = (Date.now() + timeout + 1)
-				client.getset(lockName, lockTimeoutValue, function(err, result) {
-					if(err) return retry();
-
-					if(result == existingLockTimestamp) {
-						onLockAquired(lockTimeoutValue);
-					} else {
-						retry();
-					}
-				});
-			});
-		} else {
-			onLockAquired(lockTimeoutValue);
-		}
+	client.set(lockName, lockTimeoutValue, 'PX', timeout, 'NX', function(err, result) {
+		if(err || result === null) return retry();
+		onLockAquired(lockTimeoutValue);
 	});
 }
 
@@ -57,7 +23,7 @@ module.exports = function(client, retryDelay) {
 
 	return function(lockName, timeout, taskToPerform) {
 		if(!lockName) {
-			throw new Error("You must specify a lock string. It is on the basis on this the lock is acquired.");
+			throw new Error("You must specify a lock string. It is on the redis key `lock.[string]` that the lock is acquired.");
 		}
 
 		if(!taskToPerform) {
@@ -70,7 +36,7 @@ module.exports = function(client, retryDelay) {
 		acquireLock(client, lockName, timeout, retryDelay, function(lockTimeoutValue) {
 			taskToPerform(function(done) {
 				done = done || function() {};
-				
+
 				if(lockTimeoutValue > Date.now()) {
 					client.del(lockName, done);
 				} else {
