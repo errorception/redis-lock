6fcff7beb4575b220db0e968752cc5d4c5f3d736
Adrien Guerard <adrien@cyrusbio.com>
2018-06-20 22:18:21 -0700

    Add retry timeout and callback as input parameters to the lock function

--- index.js
+++ index.js
@@ -2,18 +2,40 @@
 
 var util = require('util');
 var defaultTimeout = 5000;
+var defaultRetryTimeout = 0;
 var promisify = util.promisify || function(x) { return x; };
+var noop = (function() {});
 
-function acquireLock(client, lockName, timeout, retryDelay, onLockAcquired) {
-	function retry() {
+function acquireLock(client, lockName, timeout, retryDelay, retryTimeout, onRetryTimeout, onLockAcquired, context) {
+	function retry(context) {
 		setTimeout(function() {
-			acquireLock(client, lockName, timeout, retryDelay, onLockAcquired);
+			if(!context.didNotify) {
+				acquireLock(client, lockName, timeout, retryDelay, retryTimeout, onRetryTimeout, onLockAcquired, context);
+			}
 		}, retryDelay);
+	}
+
+	if(!context) {
+		context = {};
+		if(retryTimeout > 0) {
+			context.retryTimeoutId = setTimeout(function () {
+				context.didNotify = true;
+				onRetryTimeout();
+			}, retryTimeout);
+		}
 	}
 
 	var lockTimeoutValue = (Date.now() + timeout + 1);
 	client.set(lockName, lockTimeoutValue, 'PX', timeout, 'NX', function(err, result) {
-		if(err || result === null) return retry();
+		if(context.didNotify) {
+			return;
+		}
+		if(err || result === null) {
+			return retry(context);
+		}
+		if(context.retryTimeoutId) {
+			clearTimeout(context.retryTimeoutId);
+		}
 		onLockAcquired(lockTimeoutValue);
 	});
 }
@@ -25,7 +47,7 @@ module.exports = function(client, retryDelay) {
 
 	retryDelay = retryDelay || 50;
 
-	function lock(lockName, timeout, taskToPerform) {
+	function lock(lockName, timeout, taskToPerform, options) {
 		if(!lockName) {
 			throw new Error("You must specify a lock string. It is on the redis key `lock.[string]` that the lock is acquired.");
 		}
@@ -34,12 +56,17 @@ module.exports = function(client, retryDelay) {
 			taskToPerform = timeout;
 			timeout = defaultTimeout;
 		}
+		if(!options) {
+			options = {};
+		}
+		var retryTimeout = options.retryTimeout || defaultRetryTimeout;
+		var onRetryTimeout = options.onRetryTimeout || noop;
 
 		lockName = "lock." + lockName;
 
-		acquireLock(client, lockName, timeout, retryDelay, function(lockTimeoutValue) {
+		acquireLock(client, lockName, timeout, retryDelay, retryTimeout, onRetryTimeout, function(lockTimeoutValue) {
 			taskToPerform(promisify(function(done) {
-				done = done || function() {};
+				done = done || noop;
 
 				if(lockTimeoutValue > Date.now()) {
 					client.del(lockName, done);
@@ -51,9 +78,13 @@ module.exports = function(client, retryDelay) {
 	}
 
 	if(util.promisify) {
-		lock[util.promisify.custom] = function(lockName, timeout) {
-			return new Promise(function(resolve) {
-				lock(lockName, timeout || defaultTimeout, resolve);
+		lock[util.promisify.custom] = function(lockName, timeout, options) {
+			options = options || {};
+			return new Promise(function(resolve, reject) {
+				if(!options.onRetryTimeout) {
+					options.onRetryTimeout = reject;
+				}
+				lock(lockName, timeout || defaultTimeout, resolve, options);
 			});
 		}
 	}
